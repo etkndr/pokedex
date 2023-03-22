39d6c9ce7a93b1a7e5a21656afa40672c3a84450
Eric Kinder <ekind@Erics-MacBook-Air.local>
2023-03-21 22:42:56 -0400

    init

--- src/components/PokemonBrowser.js
+++ src/components/PokemonBrowser.js
@@ -1,16 +1,19 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
+import { getPokemon } from '../store/pokemon';
 
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
 import Fab from './Fab';
 
 const PokemonBrowser = () => {
+  const dispatch = useDispatch()
   const { pokemonId } = useParams();
-  const pokemon = useSelector(state => {
-    return state.pokemon.list.map(pokemonId => state.pokemon[pokemonId]);
-  });
+  // const pokemon = useSelector(state => {
+  //   return state.pokemon.list.map(pokemonId => state.pokemon[pokemonId]);
+  // });
+  const pokemon = dispatch(getPokemon())
   const [showForm, setShowForm] = useState(false);
 
 
